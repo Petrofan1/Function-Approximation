65a3c57975ecf2f8b1dff4a09cac2577b85a4f95
Petrofan1 <zho45@yandex.ru>
2020-04-10 20:47:25 +0300

    Действия и подсчеты вынесены в отдельные подпрограммы

---
+++ Functions of a single variable/actions.cpp
@@ -0,0 +1,162 @@
+#include "window.h"
+#include "header.hpp"
+
+int Window::command_line(int argc, char *argv[])
+{
+  if (   argc < 5
+      || sscanf (argv[1], "%lf", &a) != 1
+      || sscanf (argv[2], "%lf", &b) != 1
+      || b - a < 1.e-6
+      || sscanf (argv[3], "%d", &N) != 1
+      || N < 3
+      || sscanf(argv[4], "%d", &k) != 1
+      || (k < 0 || k > NUMBER_OF_FUNCTIONS - 1)) return -2;
+  choose_function();
+  choose_approximation();
+  set_function_array();
+  calculate_approximation();
+  update();
+  return 0;
+}
+
+void Window::reduce_n()
+{
+  N = (N/2 < 3)? 3: N/2;
+  set_function_array();
+  calculate_approximation();
+  update();
+}
+
+void Window::increase_n()
+{
+  N *= 2;
+  set_function_array();
+  calculate_approximation();
+  update();
+}
+
+
+void Window::decrease_scale()
+{
+  scale *= 0.5;
+  update();
+}
+
+void Window::increase_scale()
+{
+  scale = (scale*2 > 1)? 1: scale*2;
+  update();
+}
+void Window::choose_approximation()
+{
+  switch (approximation_id)
+  {
+    case 0:
+      approximation_name = "Newton's interpolation with multiple nodes";
+      FUNCTION = true;
+      APPROX_0 = true;
+      APPROX_1 = false;
+      RESIDUAL_0 = false;
+      RESIDUAL_1 = false;
+      break;
+    case 1:
+      approximation_name = "Piecewise interpolation by cubic Bessel polynomials";
+      FUNCTION = true;
+      APPROX_0 = false;
+      APPROX_1 = true;
+      RESIDUAL_0 = false;
+      RESIDUAL_1 = false;
+      break;
+    case 2:
+      approximation_name = "Newton's interpolation with multiple nodes + Piecewise interpolation by cubic Bessel polynomials";
+      FUNCTION = true;
+      APPROX_0 = true;
+      APPROX_1 = true;
+      RESIDUAL_0 = false;
+      RESIDUAL_1 = false;
+      break;
+    case 3:
+      approximation_name = "Errors of the first and second approximations";
+      FUNCTION = false;
+      APPROX_0 = false;
+      APPROX_1 = false;
+      RESIDUAL_0 = true;
+      RESIDUAL_1 = true;
+      break;
+  }
+}
+
+void Window::change_approximation()
+{
+  approximation_id = (approximation_id + 1) % 4;
+  choose_approximation();
+  calculate_approximation();
+  update();
+}
+
+void Window::choose_function()
+{
+  switch (k)
+  {
+    case 0:
+      function_name = "k = 0 f(x) = 1";
+      f = f_0;
+      f_der = f_der_0;
+      break;
+    case 1:
+      function_name = "k = 1 f(x) = x";
+      f = f_1;
+      f_der = f_der_1;
+      break;
+    case 2:
+      function_name = "k = 2 f(x) = x²";
+      f = f_2;
+      f_der = f_der_2;
+      break;
+    case 3:
+      function_name = "k = 3 f(x) = x^3";
+      f = f_3;
+      f_der = f_der_3;
+      break;
+    case 4:
+      function_name = "k = 4 f(x) = x^4";
+      f = f_4;
+      f_der = f_der_4;
+      break;
+    case 5:
+      function_name = "k = 5 f(x) = exp(x)";
+      f = f_5;
+      f_der = f_der_5;
+      break;
+    case 6:
+      function_name = "k = 6 f(x) = 1/(25x² + 1)";
+      f = f_6;
+      f_der = f_der_6;
+      break;
+  }
+}
+
+void Window::change_function()
+{
+  k = (k + 1) % NUMBER_OF_FUNCTIONS;
+  choose_function();
+  set_function_array();
+  calculate_approximation(); 
+  update();
+}
+
+void Window::add_delta()
+{
+  p += 1;
+  func[N/2] += 0.1*max_abs_value; 
+  calculate_approximation();
+  update();
+}
+
+void Window::subtract_delta()
+{
+  p -= 1;   
+  func[N/2] -= 0.1*max_abs_value; 
+  calculate_approximation();
+  update();
+}
---
+++ Functions of a single variable/drawing.cpp
@@ -0,0 +1,190 @@
+#include "window.h"
+#include "header.hpp"
+#define EPS 1e-20
+
+Window::Window (QWidget *parent) : QWidget (parent)
+{
+  approx_0 = new double[4];
+  approx_1 = new double[4];
+  x = new double[1];
+  func = new double[1];
+  func_der = new double[1];
+}
+
+Window::~Window(){
+    delete [] approx_0;
+    delete [] approx_1;
+    delete [] x;
+    delete [] func;
+    delete [] func_der;
+}
+
+QSize Window::minimumSizeHint() const
+{
+  return QSize(500, 500);
+}
+
+QSize Window::sizeHint() const
+{
+  return QSize(1000, 1000);
+}
+
+void Window::draw_function(QPainter *painter)
+{
+  double x1, x2, y1, y2, step;
+  x1 = a;
+  y1 = f(x1);
+  step = (b - a)/width();
+  for (x2 = x1 + step; x2 - b < step; x2 += step) 
+  {
+    y2 = f(x2);
+    painter->drawLine(QPointF(x1, y1), QPointF(x2, y2));
+    x1 = x2, y1 = y2;
+  }
+  x2 = b;
+  y2 = f(x2);
+  painter->drawLine(QPointF(x1, y1), QPointF(x2, y2));
+}
+
+void Window::draw_approximation(QPainter *painter, double *approx)
+{
+  double x1, x2, y1, y2, step;
+  x1 = a;
+  y1 = polynom_val(x1, a, b, N, approx);
+  step = (b - a)/width();
+  for (x2 = x1 + step; x2 - b < step; x2 += step) 
+  {
+    y2 = polynom_val(x2, a, b, N, approx);
+    painter->drawLine (QPointF(x1, y1), QPointF(x2, y2));
+    x1 = x2, y1 = y2;
+  }
+  x2 = b;
+  y2 = polynom_val(x2, a, b, N, approx);
+  painter->drawLine(QPointF(x1, y1), QPointF(x2, y2));
+}
+
+void Window::draw_residual(QPainter *painter, double *approx)
+{
+  double x1, x2, y1, y2, step;
+  x1 = a;
+  y1 = fabs(f(a)- polynom_val(x1, a, b, N, approx));
+  step = (b - a)/width();
+  for (x2 = x1 + step; x2 - b < step; x2 += step) 
+  {
+    y2 = fabs(f(x2) - polynom_val(x2, a, b, N, approx));
+    painter->drawLine(QPointF(x1, y1), QPointF(x2, y2));
+    x1 = x2, y1 = y2;
+  }
+  x2 = b;
+  y2 = fabs(f(x2) - polynom_val(x2, a, b, N, approx));
+  painter->drawLine(QPointF(x1, y1), QPointF(x2, y2));
+}
+
+void Window::paintEvent (QPaintEvent *)
+{  
+  QPainter painter (this);
+  double max_y, min_y, delta_y, delta, local_a, local_b;
+  local_a = (a + b)/2 - 0.5*(b - a)*scale;
+  local_b = (a + b)/2 + 0.5*(b - a)*scale; 
+
+  QPen black_pen(Qt::black, 0, Qt::SolidLine); 
+  QPen red_pen(Qt::red, 0, Qt::SolidLine); 
+  QPen blue_pen(Qt::blue, 0, Qt::SolidLine);
+  QPen green_pen(Qt::green, 0, Qt::SolidLine);
+  QPen orange_pen(QColor("orange"), 0, Qt::SolidLine);
+
+  find_min_max(approx_0, approx_1, &min_y, &max_y, scale);
+  std::cout<<"f_min = "<<min_y<<std::endl;
+  std::cout<<"f_max = "<<max_y<<std::endl;
+  std::cout<<std::endl;
+  delta_y = 0.01*(max_y - min_y);
+  if(fabs(max_y - min_y) < EPS) delta = 1;
+  else
+  {
+    min_y -= delta_y;
+    max_y += delta_y;
+    delta = max_y - min_y;
+  }
+  
+  painter.save();
+  painter.translate(0.5*width(), 0.5*height());
+  painter.scale(width()/((local_b - local_a)), -height()/(delta));
+  painter.translate(-0.5*(local_b + local_a), -0.5*(min_y + max_y));
+
+  painter.setPen(black_pen);
+  painter.drawLine(QPointF(local_a, 0), QPointF(local_b, 0));
+  painter.drawLine(QPointF(0, min_y), QPointF(0, max_y));
+  if(fabs(max_y - min_y) < EPS) painter.drawLine(QPointF(0, height()), QPointF(0, -height()));
+
+  if(FUNCTION == true)
+  {
+    painter.setPen(black_pen);
+    draw_function(&painter);
+  }
+  if(APPROX_0 == true && !(N > 50))
+  {
+    painter.setPen (blue_pen);
+    polynom_val = polynomial_value_0;
+    draw_approximation(&painter, approx_0);
+  }
+  if(APPROX_1 == true)
+  {
+    painter.setPen (red_pen);
+    polynom_val = polynomial_value_1;
+    draw_approximation(&painter, approx_1);
+  }
+  if(RESIDUAL_0 == true && !(N > 50))
+  {
+    painter.setPen(green_pen);
+    polynom_val = polynomial_value_0;
+    draw_residual(&painter, approx_0);
+  }
+  if(RESIDUAL_1 == true)
+  {
+    painter.setPen(orange_pen);
+    polynom_val = polynomial_value_1;
+    draw_residual(&painter, approx_1);
+    
+  }
+  painter.restore();
+  
+  painter.setPen(black_pen);
+  
+  QString draw_N, draw_a_b, draw_min_max, draw_scale_p;
+
+  draw_min_max.append("f_min = ");
+  draw_min_max.append(QString::number(min_y + delta_y));
+  draw_min_max.append("    f_max = ");
+  draw_min_max.append(QString::number(max_y - delta_y));
+  
+  draw_scale_p.append("Scale = ");
+  draw_scale_p.append(QString::number(1/scale));
+  draw_scale_p.append("    p = ");
+  draw_scale_p.append(QString::number(p));
+  
+  draw_a_b.append("a = ");
+  draw_a_b.append(QString::number(a));
+  draw_a_b.append("    b = ");
+  draw_a_b.append(QString::number(b));
+  
+  draw_N.append("n = ");
+  draw_N.append(QString::number(N));
+  
+  painter.drawText(10, 20, approximation_name);
+  painter.drawText(10, 40, function_name);
+  painter.drawText(10, 60, draw_N);
+  painter.drawText(10, 80, draw_a_b);
+  painter.drawText(10, 100, draw_min_max);
+  painter.drawText(10, 120, draw_scale_p);
+  
+  painter.setPen(black_pen);
+  painter.drawText(10, height() - 100, QString("Original function"));
+  painter.setPen(blue_pen);
+  painter.drawText(10, height() - 80, QString("I approximation"));
+  painter.setPen(red_pen);
+  painter.drawText(10, height() - 60, QString("II approximation"));
+  painter.setPen(green_pen);
+  painter.drawText(10, height() - 40, QString("I approximation residual"));
+  painter.setPen(orange_pen);
+  painter.drawText(10, height() - 20, QString("II approximation residual"));
+}
